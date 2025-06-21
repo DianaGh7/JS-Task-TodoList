49f235d313cad8a470e05d97c4b85eb43a525965
misk <misk.haneef@gmail.com>
2025-06-21 11:15:26 +0200

    17

--- script.js
+++ script.js
@@ -58,102 +58,14 @@ const text = taskinput.value.trim();
 
 /*-------------Misk-------------------*/
 
-let todoList = document.getElementsByClassName("todo-list")[0];
+let toDoList = document.querySelector(".todo-list");
 
-let currentTask = null;
+let toggleClass = (id,className) => document.getElementById(id).classList.toggle(className);
 
-todoList.addEventListener("click", (e) => {
-    let checkButton = e.target.closest(".check");
-
-    if(!checkButton) return;
-
-    let listItem = checkButton.closest("li");
-    let icon = checkButton.querySelector("i");
-
-    icon.classList.toggle("fa-square");
-    icon.classList.toggle("fa-check-square");
-
-    listItem.classList.toggle("completed");
-})
-
-todoList.addEventListener("click", (e) => {
-    let editButton = e.target.closest(".edit");
-
-    if(!editButton) return;
-
-    let overlay = document.getElementById("edit-overlay");
-    let opacityLayer = document.getElementById("edit-opacity");
-
-    overlay.classList.toggle("edit-overlay");
-    opacityLayer.classList.toggle("edit-opacity");
-})
-
-
-let editCancelButton = document.getElementById("edit-cancel");
-
-editCancelButton.addEventListener("click", (m) => {
-
-    let overlay = document.getElementById("edit-overlay");
-    let opacityLayer = document.getElementById("edit-opacity");
-
-    overlay.classList.toggle("edit-overlay");
-    opacityLayer.classList.toggle("edit-opacity");
+toDoList.addEventListener("click", (e) => {
+    let checkbtn = e.currentTarget.closest(".check");
     
-});
 
-let saveButton = document.getElementById("save");
-
-saveButton.addEventListener("click", (m) => {
-
-    document.getElementById("inner-todo").innerText = document.getElementById("input-edition").value;
-    
-    let overlay = document.getElementById("edit-overlay");
-    let opacityLayer = document.getElementById("edit-opacity");
-
-    overlay.classList.toggle("edit-overlay");
-    opacityLayer.classList.toggle("edit-opacity");
-    
-});
-
-
-
-
-todoList.addEventListener("click", (e) => {
-    let deleteButton = e.target.closest(".delete");
-
-    if(!deleteButton) return;
-
-    let overlay = document.getElementById("delete-overlay");
-    let opacityLayer = document.getElementById("delete-opacity");
-
-    overlay.classList.toggle("delete-overlay");
-    opacityLayer.classList.toggle("delete-opacity");
-})
-
-let confirmButton = document.getElementById("confirm");
-
-confirmButton.addEventListener("click", (m) => {
-
-
-    document.getElementById("list-item").remove();
-    let overlay = document.getElementById("delete-overlay");
-    let opacityLayer = document.getElementById("delete-opacity");
-
-    overlay.classList.toggle("delete-overlay");
-    opacityLayer.classList.toggle("delete-opacity");
-    
-});
-
-let deleteCancelButton = document.getElementById("delete-cancel");
-
-deleteCancelButton.addEventListener("click", (m) => {
-
-    let overlay = document.getElementById("delete-overlay");
-    let opacityLayer = document.getElementById("delete-opacity");
-
-    overlay.classList.toggle("delete-overlay");
-    opacityLayer.classList.toggle("delete-opacity");
-    
 });
 
 
