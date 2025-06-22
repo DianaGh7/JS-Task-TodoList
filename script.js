6b33e0a9a495133c4ac4a5a642338c6f3d48538c
Diana Ghannam <haythamghannam055@gmail.com>
2025-06-23 02:35:49 +0300

    filtering

--- script.js
+++ script.js
@@ -199,14 +199,14 @@ const showAll = () => {
 };
 
 const showDone = () => {
-  document.uerySelectorAll(".todo-list li").forEach(item => { 
+  document.querySelectorAll(".todo-list li").forEach(item => { 
     item.style.display = item.classList.contains("completed") ? "flex" : "none";
   });
 
 };
 
 const showTodo = () => {
-  document.uerySelectorAll(".todo-list li").forEach(item => { 
+  document.querySelectorAll(".todo-list li").forEach(item => { 
     item.style.display = item.classList.contains("completed") ? "none" : "flex";
   });
 
